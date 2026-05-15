5723b18cb864e5941f2a382fe02484a305869ef5
Sahil Sharma <99269975+SahilSharma-cs@users.noreply.github.com>
2026-05-15 22:42:42 +0530

    Converted Static UI into dynamic UI

---
+++ data.js
@@ -0,0 +1,35 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+        isLiked : false,
+        uuid : '46ebaff8-7136-44ac-87a0-570e88fc2dc0'
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4,
+        isLiked : false,
+        uuid : 'f7f1f2a9-c863-43ee-a540-ede422c66eb7'
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        isLiked : false,
+        uuid: '26340519-2dbc-4156-9524-e4c643207d31'
+    }
+]
--- index.js
+++ index.js
@@ -1,30 +1,65 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from  "./data.js"
 
+const postBlock = document.getElementById('post-block')
+
+document.addEventListener('click',function(e){
+    if(e.target.dataset.like){
+        handleClickLike(e.target.dataset.like)
+    }
+})
+
+function handleClickLike(postId){
+    const targetObj = posts.filter(function(post){
+        return post.uuid === postId
+    })[0]
+
+    if(targetObj.isLiked){
+        targetObj.likes--
+    }
+    else{
+        targetObj.likes++
+    }
+    targetObj.isLiked = !targetObj.isLiked
+    render()
+}
+
+function getPostHtml(){
+    let postHtml = ''
+
+    posts.forEach(function(post){
+        let heartClass = 'fa-regular'
+        let likeIconClass=''
+        if(post.isLiked){
+            heartClass = 'fa-solid'
+            likeIconClass = 'liked'
+        }
+
+        postHtml+= `<div class="post-header">
+            <img class="profile-logo" 
+                src="${post.avatar}">
+                <div class="profile-text">
+                    <h2>${post.name}</h2>
+                    <p>${post.location}</p>
+                </div>
+        </div>
+            <div class="post-image">
+                <img src="${post.post}">
+            </div>
+            <div class="post-actions">
+                <i class='${heartClass} fa-heart ${likeIconClass}' data-like="${post.uuid}"></i>
+                <i class='fa-regular fa-comment'></i>
+                <i class='fa-regular fa-paper-plane'></i>
+            </div>
+            <div class="post-info">
+                <h3>${post.likes} likes</h3>
+                <p><span class="bold-text">${post.username}</span>${post.comment}</p>
+        </div>
+        `
+    })
+    return postHtml
+}
+
+function render(){
+    postBlock.innerHTML = getPostHtml()
+}
+render()
